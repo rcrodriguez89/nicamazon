2f62ab879e5fdbd4073e5977be82e5a14a1e7c96
Roberto Rodríguez <rcrodriguez89@gmail.com>
2020-09-07 06:32:22 -0600

    Corregir el nombre de la firma del metodo de busqueda de usuario por UserDao#findByLogin

--- src/main/java/ni/edu/nicamazon/dao/UserDao.java
+++ src/main/java/ni/edu/nicamazon/dao/UserDao.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UserDao extends PagingAndSortingRepository<User, Long> {
 
-    public User findByUserName(String userName);
+    public User findByLogin(String userName);
 }
