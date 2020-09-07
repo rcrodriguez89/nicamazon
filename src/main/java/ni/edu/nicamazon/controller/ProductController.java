fcfb4792ca22172dfbb835019bcdf0b9aec3b39b
Roberto Rodríguez <rcrodriguez89@gmail.com>
2020-09-07 07:05:53 -0600

    Crear clase converter ProductEntity a ProductDTO. Crear metodos ProductController#getProducts y ProductController#getProduct

--- src/main/java/ni/edu/nicamazon/controller/ProductController.java
+++ src/main/java/ni/edu/nicamazon/controller/ProductController.java
@@ -1,14 +1,42 @@
 package ni.edu.nicamazon.controller;
 
 import ni.edu.nicamazon.dao.ProductDao;
-import org.springframework.web.bind.annotation.RestController;
+import ni.edu.nicamazon.dto.ProductDto;
+import ni.edu.nicamazon.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.inject.Inject;
+import java.util.Optional;
+import java.util.function.Function;
 
 @RestController("productController")
 public class ProductController {
-
     @Inject
     private ProductDao productDao;
 
+    @GetMapping(value="/api/Products")
+    public ResponseEntity<Page<ProductDto>> getProducts(Pageable pageable) {
+        Page<ProductDto> result = productDao.findAll(pageable).map(new ProductToProductDto());
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping(value="/api/Product/{id}")
+    public ResponseEntity<ProductDto> getProduct(@PathVariable Long id) {
+        Optional<ProductDto> result = productDao.findById(id).map(new ProductToProductDto());
+        return new ResponseEntity<>(result.get(), HttpStatus.OK);
+    }
+
+    private class ProductToProductDto implements Function<Product, ProductDto> {
+        @Override
+        public ProductDto apply(Product product) {
+
+            return new ProductDto(product.getId(), product.getTitle(), product.getPrice(), product.isSpecial(), product.getDescription(), product.getCategory(),
+                    String.format("/api/Product/%d/thumbnail", product.getId()),
+                    String.format("/api/Product/%d/file", product.getId()));
+        }
+    }
 }
