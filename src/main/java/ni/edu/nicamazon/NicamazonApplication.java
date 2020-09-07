bfcd5b70916e457a9292c222a4f4a0f2633e3e5f
Roberto Rodríguez <rcrodriguez89@gmail.com>
2020-09-06 18:49:09 -0600

    Crear proyecto Spring Boot con Gradle

---
+++ src/main/java/ni/edu/nicamazon/NicamazonApplication.java
@@ -0,0 +1,13 @@
+package ni.edu.nicamazon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NicamazonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicamazonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ni/edu/nicamazon/NicamazonApplicationTests.java
@@ -0,0 +1,13 @@
+package ni.edu.nicamazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NicamazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
